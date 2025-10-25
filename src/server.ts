74c9fd4e5bee2f62a2b8dd3c9394aa4c40b66c95
unknown <md54715@fer.hr>
2025-10-26 00:45:26 +0200

    provjera povezanosti-2

--- src/server.ts
+++ src/server.ts
@@ -15,9 +15,9 @@ const app = express();
 });
  */
 
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs"); 
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.set("views", "./views");
+app.use(express.static('./public')); 
 
 app.get('/', (req, res) => {
   res.render('index');
