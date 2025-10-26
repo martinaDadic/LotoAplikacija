4c300a4d67b16b4eaa6f152fa0e6cb8bcbc15b22
unknown <md54715@fer.hr>
2025-10-26 15:20:28 +0100

    dodan login

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import https from 'https'
 import fs from 'fs' //modul za rad s fileovima
 import path from "path";
 import { fileURLToPath } from "url";
+import { auth } from 'express-openid-connect';
 //import { getComments } from './db.ts';
 
 dotenv.config()
@@ -19,22 +20,31 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const externalUrl = process.env.RENDER_EXTERNAL_URL; //uzima javni URL sa rendera, ako postoji
+const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: externalUrl || `http://localhost:${port}`, //externalUrl || `http://localhost:${port}`
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: 'https://dev-i25ptmtk6aiqoev1.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  //res.render('index');
 });
 
-const externalUrl = process.env.RENDER_EXTERNAL_URL; //uzima javni URL sa rendera, ako postoji
-const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
-
-const config = {
-  baseURL: externalUrl || `https://localhost:${port}`,
-};
-
-if (false) { //externalUrl
+if (externalUrl) { //externalUrl
   const hostname = '0.0.0.0';
   app.listen(port, hostname, () => {
     console.log(`Server locally running at http://${hostname}:${port}/ and from
